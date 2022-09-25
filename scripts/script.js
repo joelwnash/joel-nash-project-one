ab04d05a062974d93febab42d474346190c78cca
joelwnash <joel.w.nash@gmail.com>
2022-09-24 21:22:32 -0400

    image carousel completed

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 // Hamburger Menu
 // This hamburger menu makes extensive use of markup/code shared by ljc-dev at https://dev.to/ljcdev/easy-hamburger-menu-with-js-2do0
 
-
 const menu = document.querySelector(".navbar ul");
 const menuItems = document.querySelectorAll(".menu-item");
 const hamburgerButtons = document.querySelector(".hamburger-menu");
@@ -27,5 +26,40 @@ menuItems.forEach(
 )
 
 hamburgerButtons.addEventListener("click", toggleHamburger);
+
+
+
+
+// Image Carousel Feature
+// Iamge carousel is based on article by Jemima Abu at https://webdesign.tutsplus.com/tutorials/how-to-build-a-simple-carousel-with-vanilla-javascript--cms-41734
+
+const slidesContainer = document.querySelector(".slides-container");
+const slide = document.querySelector(".slide");
+const slideWidth = slide.clientWidth;
+const prevButton = document.querySelector("#slide-arrow-previous");
+const nextButton = document.querySelector("#slide-arrow-next");
+
+const checkButtons = function() {
+    if ((slidesContainer.scrollLeft * 2) > slidesContainer.scrollWidth) {
+        nextButton.classList.add("subtle-arrow");
+    } else if (slidesContainer.scrollLeft < 25){
+        prevButton.classList.add("subtle-arrow");
+    } else {
+        prevButton.classList.remove("subtle-arrow");
+        nextButton.classList.remove("subtle-arrow");
+    };
+};
+
+nextButton.addEventListener("click", (event) => {
+    slidesContainer.scrollLeft += slideWidth;
+    setTimeout(checkButtons, 300);
+});
+
+prevButton.addEventListener("click", (event) => {
+    slidesContainer.scrollLeft -= slideWidth;
+    setTimeout(checkButtons, 300);
+});
+
+
 
 
